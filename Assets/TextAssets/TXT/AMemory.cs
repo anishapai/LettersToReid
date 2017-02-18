c5e91691847c278742665c4227a65ff2285cb96c
Isaiah Mann <imann24@gmail.com>
2017-02-18 12:15:23 -0500

    Memory Parsing code

---
+++ Assets/TextAssets/TXT/AMemory.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/* Class that defines 
+ * 
+*/
+
+[System.Serializable]
+public class AMemory {
+
+    public int id;
+    public string body;
+		
+
+}
